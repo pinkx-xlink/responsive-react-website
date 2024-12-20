5c2d4faa788d5d6fc014979db287484f6742e8d3
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-20 15:20:39 -0500

    Create products and sign up tabs

--- responsive-website/src/components/Navbar.js
+++ responsive-website/src/components/Navbar.js
@@ -24,8 +24,18 @@ function Navbar() {
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
                         Services
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                        Products
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                        Sign Up
                         </Link>
                     </li>
                 </ul>
