85f452fd382883b60ad03e3be4d3d6cc81485cc2
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-20 14:50:53 -0500

    Import BrowserRouter to resolve render error

--- responsive-website/src/App.js
+++ responsive-website/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar />
+      </BrowserRouter>
+      
     </div>
   );
 }
