c69fb32f33ccc55d84089e21b7f2813bf8667614
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-20 16:32:34 -0500

    Import Google font

--- responsive-website/src/components/Button.js
+++ responsive-website/src/components/Button.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, 
-    buttonStyle, buttonSize}) => {
-        const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle 
+export const Button = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+        const checkButtonStyle = STYLES.includes(buttonStyle) 
+        ? buttonStyle 
         : STYLES[0];
 
         const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
@@ -23,5 +29,5 @@ export const Button = ({children, type, onClick,
                     {children}
                 </button>
             </Link>
-        )
+        );
     };
